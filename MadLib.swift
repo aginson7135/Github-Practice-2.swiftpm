c71179c7583730ecf8d8b648023c2f7526948861
Nicole Park <ypark5275@stu.d214.org>
2024-04-15 14:39:46 -0500

    created new file and a struct named madlib

---
+++ MadLib.swift
@@ -0,0 +1,14 @@
+//
+//  MadLib.swift
+//  Github Practice 2
+//
+//  Created by Nicole Park on 4/15/24.
+//
+
+import Foundation
+
+struct MadLib {
+    var noun: String
+    var verb: String
+    var adjective: String
+}
