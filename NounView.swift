3bad8184f69c26f4ed3611a862e0e6db73e0610b
Nicole Park <ypark5275@stu.d214.org>
2024-04-15 14:18:57 -0500

    created a noun view and added a navigation link to verb view.

---
+++ NounView.swift
@@ -0,0 +1,14 @@
+//
+//  NounView.swift
+//  Github Practice 2
+//
+//  Created by Nicole Park on 4/15/24.
+//
+
+import Foundation
+import SwiftUI
+struct NounView: View {
+    var body: some View {
+        NavigationLink("Go to VerbView", destination: VerbView())
+    }
+}
