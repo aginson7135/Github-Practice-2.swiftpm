986cd16f3f33cbff78343ddc21154cede7953d23
Aneena Ginson <aginson7135@stu.d214.org>
2024-04-15 14:26:52 -0500

    added verb view

---
+++ VerbView.swift
@@ -0,0 +1,14 @@
+//
+//  VerbView.swift
+//  Github Practice 2
+//
+//  Created by Aneena Ginson on 4/15/24.
+//
+
+import Foundation
+import SwiftUI
+struct VerbView: View {
+    var body: some View {
+        NavigationLink("Go to AdjectiveView", destination: AdjectiveView())
+    }
+}
